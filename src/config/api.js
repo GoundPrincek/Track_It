517ee249d5de1ea7afaa96036caa5ec50412f71d
Design_Practice <Princestudy.7498@gmail.com>
2026-03-29 21:13:26 +0530

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
-const HOST = "192.168.0.103";
+const DEV_API_BASE = "http://127.0.0.1:5000/api";
 
-export const API_BASE = `http://${HOST}:5000/api`;
+export const API_BASE = import.meta.env.VITE_API_BASE || DEV_API_BASE;
